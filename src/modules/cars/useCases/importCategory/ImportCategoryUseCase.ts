4162d5936a11b56f276afed30828fc0ca65c1790
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-01-15 08:56:22 -0300

    update return type from loadCategories()

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -10,7 +10,7 @@ interface IImportCategory {
 class ImportCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    private loadCategories(file: Express.Multer.File): Promise<IImportCategory> {
+    private loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
         return new Promise((resolve, reject) => {
             const stream = fs.createReadStream(file.path)
 
