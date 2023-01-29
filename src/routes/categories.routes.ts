623e08f8c005b671efe7f7a5ec1ac8266e214fa4
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-01-29 11:19:43 -0300

    refactor list categories with tsyringe

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import multer from 'multer'
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController'
-import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+import { ListCategoriesController } from '../modules/cars/useCases/listCategories/ListCategoriesController'
 import { ImportCategoryController } from '../modules/cars/useCases/importCategory/ImportCategoryController'
 
 const categoriesRoutes = Router()
@@ -12,12 +12,11 @@ const upload = multer({
 
 const createCategoryController = new CreateCategoryController()
 const importCategoryController = new ImportCategoryController()
+const listCategoriesController = new ListCategoriesController()
 
 categoriesRoutes.post('/', createCategoryController.handle)
 
-categoriesRoutes.get('/', (request, response) => {
-	return listCategoriesController.handle(request, response)
-})
+categoriesRoutes.get('/', listCategoriesController.handle)
 
 categoriesRoutes.post('/import', upload.single('file'), importCategoryController.handle)
 
