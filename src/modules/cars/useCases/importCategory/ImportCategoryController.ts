6b3703237e19c4a9adefb523f414cfc54fa18bd7
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-01-31 05:52:42 -0300

    update doc and change status from import category

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -10,7 +10,7 @@ class ImportCategoryController {
 
         await importCategoryUseCase.execute(file)
         
-        return response.status(200).send()
+        return response.status(201).send()
     }
 }
 
