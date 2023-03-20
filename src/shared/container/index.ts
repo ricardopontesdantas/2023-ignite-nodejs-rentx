4bc417601d559ce02293145adf4e180186a8298f
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-03-20 08:06:39 -0300

    add cars repository in container dependency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,8 @@ import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecifica
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository'
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository'
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository'
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository'
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -20,4 +22,9 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
     'UsersRepository',
     UsersRepository
+)
+
+container.registerSingleton<ICarsRepository>(
+    'CarsRepository',
+    CarsRepository
 )
