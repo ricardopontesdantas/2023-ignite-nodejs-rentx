fcd83f4de26e6ec74a2fd688da49d8e591fafd69
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-03-24 08:09:47 -0300

    feat(repository/cars): create a method to return available cars also using filters

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -9,7 +9,7 @@ class CarsRepository implements ICarsRepository {
     constructor() {
         this.repository = getRepository(Car)
     }
-
+    
     async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<Car> {
         const car = this.repository.create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name })
         await this.repository.save(car)
@@ -19,6 +19,28 @@ class CarsRepository implements ICarsRepository {
     async findByLicensePlate(license_plate: string): Promise<Car> {
         const car = await this.repository.findOne({ license_plate })
         return car
+    }
+
+    async findAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]> {
+        const carsQuery = await this.repository
+            .createQueryBuilder('c')
+            .where('available = :available', { available: true })
+        
+        if(brand) {
+            carsQuery.andWhere('c.brand = :brand', { brand })
+        }
+
+        if(name) {
+            carsQuery.andWhere('c.name = :name', { name })
+        }
+
+        if(category_id) {
+            carsQuery.andWhere('c.category_id = :category_id', { category_id })
+        }
+
+        const cars = await carsQuery.getMany()
+
+        return cars
     }
 }
 
