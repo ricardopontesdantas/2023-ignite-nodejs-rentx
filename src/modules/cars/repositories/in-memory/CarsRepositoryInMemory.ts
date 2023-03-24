ad2e0a706683a6d9d32392b89b58f4bf5141a671
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-03-24 08:07:43 -0300

    fix(repository/in-memory): fix conditional to return available cars

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -34,7 +34,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
     ): Promise<Car[]> {
         const cars = this.cars.filter(car => {
             if(
-                car.available === true &&
+                car.available === true ||
                 ((brand && car.brand === brand) || 
                 (category_id && car.category_id === category_id) || 
                 (name && car.name === name))
