38e50dee8ab0448850cc2e54c412f7af0e6cd1fd
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-03-27 07:56:11 -0300

    feat(usecase/create-cars-specifications): find specifications em create a car

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -8,7 +9,8 @@ interface IRequest {
 
 class CreateCarSpecificationUseCase {
     constructor(
-        private carsRepository: ICarsRepository
+        private carsRepository: ICarsRepository,
+        private specificationsRepository: ISpecificationsRepository
     ) {}
     
     async execute({ car_id, specifications_id }: IRequest): Promise<void> {
@@ -17,6 +19,14 @@ class CreateCarSpecificationUseCase {
         if(!carsExists) {
             throw new AppError('Car does not exists')
         }
+
+        const specifications = await this.specificationsRepository.findByIds(specifications_id)
+
+        carsExists.specifications = specifications
+
+        await this.carsRepository.create(carsExists)
+
+        console.log(carsExists)
     }
 }
 
