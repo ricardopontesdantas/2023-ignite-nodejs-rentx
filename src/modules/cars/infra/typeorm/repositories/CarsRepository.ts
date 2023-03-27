c2a307d1fb411ff0baa944272de304b7dc5d38bb
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-03-27 07:52:19 -0300

    feat(repository/cars): add specifications property

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -10,8 +10,8 @@ class CarsRepository implements ICarsRepository {
         this.repository = getRepository(Car)
     }
     
-    async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<Car> {
-        const car = this.repository.create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name })
+    async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name, specifications }: ICreateCarDTO): Promise<Car> {
+        const car = this.repository.create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name, specifications })
         await this.repository.save(car)
         return car
     }
@@ -44,7 +44,7 @@ class CarsRepository implements ICarsRepository {
     }
 
     async findById(id: string): Promise<Car> {
-        const car = await this.repository.findOne({ id })
+        const car = await this.repository.findOne(id)
         return car
     }
 }
