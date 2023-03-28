6568206ae94b8a99ab492e37683c50ac78452a39
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-03-28 07:15:54 -0300

    feat(usecase/craete-car-specification): return a car on execute method and inject dependencies

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,19 +1,25 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { AppError } from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
     car_id: string
     specifications_id: string[]
 }
 
+@injectable()
 class CreateCarSpecificationUseCase {
     constructor(
+        @inject('CarsRepository')
         private carsRepository: ICarsRepository,
+
+        @inject('SpecificationsRepository')
         private specificationsRepository: ISpecificationsRepository
     ) {}
     
-    async execute({ car_id, specifications_id }: IRequest): Promise<void> {
+    async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
         const carsExists = await this.carsRepository.findById(car_id)
 
         if(!carsExists) {
@@ -26,7 +32,7 @@ class CreateCarSpecificationUseCase {
 
         await this.carsRepository.create(carsExists)
 
-        console.log(carsExists)
+        return carsExists
     }
 }
 
