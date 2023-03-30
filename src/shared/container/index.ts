96c7d8620487bda8effc6ef1d3439993bf98aaf4
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-03-30 06:42:57 -0300

    feat(container): register repository cars images to dependency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,8 @@ import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/C
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository'
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository'
+import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository'
+import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository'
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -27,4 +29,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ICarsRepository>(
     'CarsRepository',
     CarsRepository
+)
+
+container.registerSingleton<ICarsImagesRepository>(
+    'CarsImagesRepository',
+    CarsImagesRepository
 )
