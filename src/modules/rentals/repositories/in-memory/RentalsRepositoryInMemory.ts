909162368434701c712cd88a781785c6f1bbea8c
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-01 07:43:56 -0300

    feat(repository/rentals-in-memory): create a rentals repository in memory

---
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -0,0 +1,16 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "../IRentalsRepository";
+
+class RentaslRepositoryInMemory implements IRentalsRepository {
+    rentals: Rental[] = []
+
+    async findOpenRentalByCar(car_id: string): Promise<Rental> {
+        return this.rentals.find(rental => rental.car_id === car_id && !rental.end_date)
+    }
+
+    async findOpenRentalByUse(user_id: string): Promise<Rental> {
+        return this.rentals.find(rental => rental.user_id === user_id && !rental.end_date)
+    }
+}
+
+export { RentaslRepositoryInMemory }
