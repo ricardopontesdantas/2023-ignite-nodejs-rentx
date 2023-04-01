199ee0dd7349dc05b3457efbad215c46e87d0cf3
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-01 07:44:53 -0300

    test(usecase/create-rental): create rental use case

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,19 @@
+import { RentaslRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory"
+import { CreateRentalUseCase } from "./CreateRentalUseCase"
+
+let createRentalUseCase: CreateRentalUseCase
+let rentalsRepositoryInMemory: RentaslRepositoryInMemory
+
+describe('Create Rental', () => {
+    beforeEach(() => {
+        rentalsRepositoryInMemory = new RentaslRepositoryInMemory()
+        createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory)
+    })
+
+    it('should be able to create a new rental', async () => {
+        const car_id = '12345'
+        const user_id = '121212'
+        const expected_return_date = new Date()
+        await createRentalUseCase.execute({ car_id, user_id, expected_return_date })
+    })
+})
