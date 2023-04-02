96d35c024a1c6a07de3c7cb458404127a75dbf70
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-02 08:30:01 -0300

    test(create-rental): tests not able to create new rental

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentaslRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory"
+import { AppError } from "@shared/errors/AppError"
 import { CreateRentalUseCase } from "./CreateRentalUseCase"
 
 let createRentalUseCase: CreateRentalUseCase
@@ -18,5 +19,26 @@ describe('Create Rental', () => {
 
         expect(rental).toHaveProperty('id')
         expect(rental).toHaveProperty('start_date')
+    })
+
+    it('should not be able to create a new rental if there is another open to the same user', async () => {
+        expect(async () => {
+            const car_id = '12345'
+            const user_id = '121212'
+            const expected_return_date = new Date()
+            await createRentalUseCase.execute({ car_id, user_id, expected_return_date })
+            await createRentalUseCase.execute({ car_id, user_id, expected_return_date })
+        }).rejects.toBeInstanceOf(AppError)
+    })
+
+    it('should not be able to create a new rental if there is another open to the same car', async () => {
+        expect(async () => {
+            const car_id = '12345'
+            const user_id_1 = '123'
+            const user_id_2 = '321'
+            const expected_return_date = new Date()
+            await createRentalUseCase.execute({ car_id, user_id: user_id_1, expected_return_date })
+            await createRentalUseCase.execute({ car_id, user_id: user_id_2, expected_return_date })
+        }).rejects.toBeInstanceOf(AppError)
     })
 })
