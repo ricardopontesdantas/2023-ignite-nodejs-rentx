36059acf888dd984e405915102a428d7e348c50e
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-02 08:17:04 -0300

    feat(repository/rentals-in-memory): implements create method

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,3 +1,4 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../IRentalsRepository";
 
@@ -10,6 +11,21 @@ class RentaslRepositoryInMemory implements IRentalsRepository {
 
     async findOpenRentalByUse(user_id: string): Promise<Rental> {
         return this.rentals.find(rental => rental.user_id === user_id && !rental.end_date)
+    }
+
+    async create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+        const rental = new Rental()
+
+        Object.assign(rental, {
+            user_id,
+            car_id,
+            expected_return_date,
+            start_date: new Date()
+        })
+
+        this.rentals.push(rental)
+
+        return rental
     }
 }
 
