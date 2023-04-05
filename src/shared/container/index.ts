ae9319a4e05ebcc0c6d0ea97c831295956e8768a
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-05 06:51:10 -0300

    feat(container): register rentals repository and import provider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe'
 
+import '@shared/container/providers'
+
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository'
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository'
@@ -10,6 +12,8 @@ import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository'
 import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository'
 import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository'
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository'
+import { RentalsRepository } from '@modules/rentals/infra/typeorm/repositories/RentalsRepository'
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -34,4 +38,9 @@ container.registerSingleton<ICarsRepository>(
 container.registerSingleton<ICarsImagesRepository>(
     'CarsImagesRepository',
     CarsImagesRepository
+)
+
+container.registerSingleton<IRentalsRepository>(
+    'RentalsRepository',
+    RentalsRepository
 )
