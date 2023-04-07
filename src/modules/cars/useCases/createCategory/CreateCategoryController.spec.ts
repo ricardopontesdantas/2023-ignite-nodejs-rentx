7b15ee2b829aca51763e957341ae70cc4325dafd
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-07 08:02:06 -0300

    test(controller/create-category): create connection and session to run the test

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,12 +1,48 @@
 import { app } from '@shared/infra/http/app'
 import request from 'supertest'
+import createConnection from '@shared/infra/typeorm'
+import { Connection } from 'typeorm'
+import { hash } from 'bcryptjs'
+import { v4 as uuidv4 } from 'uuid'
+
+let connection: Connection
 
 describe('Create Category Controller', () => {
+    beforeAll(async () => {
+        connection = await createConnection()
+        await connection.runMigrations()
+
+        const id = uuidv4()
+        const password = await hash('admin', 8)
+        
+        await connection.query(
+            `INSERT INTO users(id, name, email, password, "isAdmin", created_at, driver_license)
+            VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'xxxxxxx')`
+        )
+    })
+
+    afterAll(async () => {
+        await connection.dropDatabase()
+        await connection.close()
+    })
+
     it('should be able to create a new category', async () => {
-        const response = await request(app).post('/categories').send({
-            name: 'Category Supertest',
-            description: 'Description Category Supertest'
+        const responseToken = await request(app).post('/sessions').send({
+            email: 'admin@rentx.com.br',
+            password: 'admin'
         })
+
+        const { token } = responseToken.body
+
+        const response = await request(app)
+            .post('/categories')
+            .send({
+                name: 'Category Supertest',
+                description: 'Description Category Supertest'
+            })
+            .set({
+                Authorization: `Bearer ${token}`
+            })
 
         expect(response.status).toBe(201)
     })
