f43d987f83729767ef4b3dda99d0538f8473d2a7
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-08 09:57:41 -0300

    test(controller/list-categories): should be able to list all categories

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,54 @@
+import { app } from '@shared/infra/http/app'
+import request from 'supertest'
+import createConnection from '@shared/infra/typeorm'
+import { Connection } from "typeorm"
+import { hash } from 'bcryptjs'
+import { v4 as uuidv4 } from 'uuid'
+
+let connection: Connection
+
+describe('List Categories', () => {
+    beforeAll(async () => {
+        connection = await createConnection()
+        await connection.runMigrations()
+
+        const id = uuidv4()
+        const password = await hash('admin', 8)
+        
+        await connection.query(
+            `INSERT INTO users(id, name, email, password, "isAdmin", created_at, driver_license)
+            VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'xxxxxxx')`
+        )
+    })
+
+    afterAll(async () => {
+        await connection.dropDatabase()
+        await connection.close()
+    })
+
+    it('shoud be able to list all categories', async () => {
+        const responseToken = await request(app).post('/sessions').send({
+            email: 'admin@rentx.com.br',
+            password: 'admin'
+        })
+
+        const { token } = responseToken.body
+
+        await request(app)
+            .post('/categories')
+            .send({
+                name: 'Category Supertest',
+                description: 'Description Category Supertest'
+            })
+            .set({
+                Authorization: `Bearer ${token}`
+            })
+        
+        const response = await request(app).get('/categories')
+
+        expect(response.status).toBe(200)
+        expect(response.body.length).toBe(1)
+        expect(response.body[0]).toHaveProperty('id')
+        expect(response.body[0].name).toEqual('Category Supertest')
+    })
+})
