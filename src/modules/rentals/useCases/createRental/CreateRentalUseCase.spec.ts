d53b2e28147475c579f2a82a4d3152e92f01cfbd
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-14 07:20:22 -0300

    test(usecase/create-rental): pass cars repository in memory to constructor

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -2,18 +2,21 @@ import { RentaslRepositoryInMemory } from "@modules/rentals/repositories/in-memo
 import { AppError } from "@shared/errors/AppError"
 import { CreateRentalUseCase } from "./CreateRentalUseCase"
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider"
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory"
 import dayjs from "dayjs"
 
 let createRentalUseCase: CreateRentalUseCase
 let rentalsRepositoryInMemory: RentaslRepositoryInMemory
+let carsRepositoryInMemory: CarsRepositoryInMemory
 let dayjsDateProvider: DayjsDateProvider
 
 describe('Create Rental', () => {
     const dayAdd24Hours = dayjs().add(1, 'day').toDate()
     beforeEach(() => {
         rentalsRepositoryInMemory = new RentaslRepositoryInMemory()
+        carsRepositoryInMemory = new CarsRepositoryInMemory()
         dayjsDateProvider = new DayjsDateProvider()
-        createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory, dayjsDateProvider)
+        createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory, dayjsDateProvider, carsRepositoryInMemory)
     })
 
     it('should be able to create a new rental', async () => {
