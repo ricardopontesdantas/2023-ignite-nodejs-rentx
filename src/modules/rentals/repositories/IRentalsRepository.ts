f358cae92920e3931910744cd375cf9e038fc481
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-15 08:01:32 -0300

    feat(repository/irentals): add findById method

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,6 +5,7 @@ interface IRentalsRepository {
     findOpenRentalByCar(car_id: string): Promise<Rental>
     findOpenRentalByUse(user_id: string): Promise<Rental>
     create(data: ICreateRentalDTO): Promise<Rental>
+    findById(id: string): Promise<Rental>
 }
 
 export { IRentalsRepository }
