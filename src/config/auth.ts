1a34269de72387f7c6397eabe5aa6f9d3405bf95
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-22 07:59:47 -0300

    feat(config/auth): create configs for refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: '69c6e4a394b794ac45a4ca63761bff01',
+    expires_in_token: '15m',
+    secret_refresh_token: '14075fd61fcfba54adfc181fc08b7dfb74f3c208',
+    expires_in_refresh_token: '30d',
+    expires_refresh_token_days: 30
+}
