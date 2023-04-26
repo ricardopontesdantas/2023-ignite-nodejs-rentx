a0518c4750c3329becf18c85e53131bf18c0a984
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-26 14:14:51 -0300

    feat(routes):import password route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,6 +5,7 @@ import { categoriesRoutes } from './categories.routes'
 import { rentalRoutes } from './rental.routes'
 import { specificationsRoutes } from './specifications.routes'
 import { usersRoutes } from './users.routes'
+import { passwordRoutes } from './password.routes'
 
 const router = Router()
 
@@ -13,6 +14,7 @@ router.use('/specifications', specificationsRoutes)
 router.use('/users', usersRoutes)
 router.use('/cars', carsRoutes)
 router.use('/rentals', rentalRoutes)
+router.use('/password' ,passwordRoutes)
 router.use(authenticateRoutes)
 
 export { router }
