ed01f3b90c329116f9613bf60f528d21b73dcaaf
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-28 08:28:40 -0300

    feat(controller/reset-password): create reset password user controller

---
+++ src/modules/accounts/useCases/resetPassword/ResetPasswordUserController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { ResetPasswordUserUseCase } from "./ResetPasswordUserUseCase";
+
+class ResetPasswordUseController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { token } = request.query
+        const { password } = request.body
+        const resetPasswordUserUseCase = container.resolve(ResetPasswordUserUseCase)
+        await resetPasswordUserUseCase.execute({ token: String(token), password })
+        return response.end()
+    }
+}
+
+export { ResetPasswordUseController }
