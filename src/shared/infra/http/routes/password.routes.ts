0ec35599172082a7cee536077eafee207581561c
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-04-28 08:30:17 -0300

    refactor(routes/password): change 'use' to 'user'

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,11 @@
-import { ResetPasswordUseController } from '@modules/accounts/useCases/resetPassword/ResetPasswordUserController'
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPassword/ResetPasswordUserController'
 import { SendForgotPasswordMailController } from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController'
 import { Router } from 'express'
 
 const passwordRoutes = Router()
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController()
-const resetPasswordUserController = new ResetPasswordUseController()
+const resetPasswordUserController = new ResetPasswordUserController()
 
 passwordRoutes.post('/forgot', sendForgotPasswordMailController.handle)
 passwordRoutes.post('/reset', resetPasswordUserController.handle)
