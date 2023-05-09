0f18bf84af68507aef67bce0d8427065c6b935d2
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-05-09 06:16:00 -0300

    feat(container/provider/index): add disk storage in storage provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,6 +5,7 @@ import { IMailProvider } from './MailProvider/IMailProvider'
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider'
 import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider'
 import { IStorageProvider } from './StorageProvider/IStorageProvider'
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider'
 
 container.registerSingleton<IDateProvider>(
     'DayjsDateProvider',
@@ -16,7 +17,12 @@ container.registerInstance<IMailProvider>(
     new EtherealMailProvider()
 )
 
+const diskStorage = {
+    local: LocalStorageProvider,
+    s3: S3StorageProvider
+}
+
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
-    LocalStorageProvider
+    diskStorage[process.env.DISK]
 )
