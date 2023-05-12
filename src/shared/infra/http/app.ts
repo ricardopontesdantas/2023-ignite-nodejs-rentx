b96ff34f3cc091dca545c1b1e1a8a584523bb353
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-05-12 06:27:04 -0300

    feat(http/app): config static routes to avatar and cars

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata'
+import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
@@ -9,6 +10,7 @@ import { router } from './routes'
 import '@shared/container'
 import { AppError } from '@shared/errors/AppError'
 import createConnection from '@shared/infra/typeorm'
+import upload from '@config/upload';
 
 createConnection()
 const app = express()
@@ -16,6 +18,9 @@ const app = express()
 app.use(express.json())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`))
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`))
 
 app.use(router)
 
