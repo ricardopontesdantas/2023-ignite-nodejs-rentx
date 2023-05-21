a1bf771f34a5bbaa04f2c7901571b73c15aae653
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-05-21 07:59:42 -0300

    feat(app): add middleware cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -11,6 +11,7 @@ import '@shared/container'
 import { AppError } from '@shared/errors/AppError'
 import createConnection from '@shared/infra/typeorm'
 import upload from '@config/upload';
+import cors from 'cors';
 
 createConnection()
 const app = express()
@@ -22,6 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`))
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`))
 
+app.use(cors())
 app.use(router)
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
